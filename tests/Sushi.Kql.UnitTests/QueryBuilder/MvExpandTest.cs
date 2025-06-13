f605e84f6a2053ad0a5749ee3216b5f69c1fd6cb
Koen van der Meer <koen.vandermeer@supershift.nl>
2025-06-13 16:36:31 +0200

    remove duplicate integration tests add unit tests for query builders

---
+++ tests/Sushi.Kql.UnitTests/QueryBuilder/MvExpandTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sushi.Kql.TimeSeries;
+
+namespace Sushi.Kql.UnitTests.QueryBuilder;
+public class MvExpandTest
+{
+    private readonly QueryBuilder<SalesFact> _queryBuilder;
+    public MvExpandTest()
+    {
+        _queryBuilder = new QueryBuilder<SalesFact>(new SalesFactMap());
+    }
+
+    [Fact]
+    public void ExpandTimeSeries()
+    {
+        int days = 14;
+        var from = new DateTime(2007, 04, 01, 0, 0, 0, DateTimeKind.Utc);
+        var to = from.AddDays(days);
+
+        string countAlias = "SalesCount";
+
+        _queryBuilder.Select().MakeSeries().Agg(a => a.Count(countAlias)).On(x => x.DateKey).From(from).To(to).Step("1d");
+        _queryBuilder.MvExpand().Add(countAlias, KqlDataType.Long).Add(x => x.DateKey);
+        var kqlQuery = _queryBuilder.ToKqlString();
+
+        // Assert
+        var expectedMakeSeries = @"| make-series SalesCount = count() on DateKey from p0 to p1 step 1d| mv-expand SalesCount to typeof(long),DateKey to typeof(datetime)";
+        Assert.Contains(expectedMakeSeries, kqlQuery);
+    }
+}
---
+++ tests/Sushi.Kql.UnitTests/SalesFactMap.cs
@@ -0,0 +1,15 @@
+﻿using Sushi.Kql.Mapping;
+
+namespace Sushi.Kql.UnitTests;
+public class SalesFactMap : DataMap<SalesFact>
+{
+    public SalesFactMap()
+    {
+        Table("SalesFact");
+        Map(x => x.CustomerKey);
+        Map(x => x.DateKey);
+        Map(x => x.ProductKey);
+        Map(x => x.SalesAmount);
+        Map(x => x.TotalCost);
+    }
+}
