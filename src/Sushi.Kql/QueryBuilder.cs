4bfeb5d83971af4c4cb12cbe2858d1c271a7ba67
Koen van der Meer <koen.vandermeer@supershift.nl>
2025-06-23 13:13:18 +0200

    use expression with Top

--- src/Sushi.Kql/QueryBuilder.cs
+++ src/Sushi.Kql/QueryBuilder.cs
@@ -102,6 +102,16 @@ public class QueryBuilder<T> : IQueryBuilder
     }
 
     /// <summary>
+    /// Limits the results to a specified number of rows.
+    /// </summary>        
+    /// <returns></returns>
+    public void Top(int numberOfRows, Expression<Func<T, object?>> column, SortDirection? sortDirection = null)
+    {
+        var dataMapItem = _map.GetItem(column);
+        Top(numberOfRows, dataMapItem.Column, sortDirection);
+    }
+
+    /// <summary>
     /// Binds a name to all previous input in the query, allowing it to be referenced later in the query by the name.
     /// </summary>
     /// <param name="name"></param>
